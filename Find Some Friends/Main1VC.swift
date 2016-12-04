0d587b720cad227101bdbf7280e6b24de4afa3b3
John Leonardo <jdleonardo5@icloud.com>
2016-12-03 23:27:06 -0800

    collection views working kinda

--- Find Some Friends/Main1VC.swift
+++ Find Some Friends/Main1VC.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class Main1VC: UIViewController {
+class Main1VC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    @IBOutlet weak var bgView: UIView!
     var userID: String!
+    
+    let reuse = "UserCell"
+    var users = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.bgView.backgroundColor = UIColor(patternImage: UIImage(named: "bg.png")!)
         print(userID)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuse, for: indexPath as IndexPath) as! UserCell
+        cell.profileImg.image = UIImage(named: "smile.png")
+        return cell
     }
 
 }
---
+++ Find Some Friends/UserCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserCell.swift
+//  Find Some Friends
+//
+//  Created by John Leonardo on 12/3/16.
+//  Copyright © 2016 John Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UICollectionViewCell {
+    @IBOutlet weak var profileImg: UIImageView!
+    
+    
+}
