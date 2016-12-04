0ee27f69a5349b46d7fc3746a83f8b57d9054878
John Leonardo <jdleonardo5@icloud.com>
2016-12-03 16:36:27 -0800

    outlets

--- Find Some Friends/LoginVC.swift
+++ Find Some Friends/LoginVC.swift
@@ -10,20 +10,28 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
         if launchedBefore  {
             print("Not the first launch")
         } else {
             
             print("First launch, displaying welcome message")
-            
+            let aC = UIAlertController(title: "Welcome", message: "Welcome to FSF! Feel free to login/signup here. If you don't have an account, it will be created automatically. If you do, just sign in as usual. You will then be prompted to set a profile picture. Have fun! -John", preferredStyle: UIAlertControllerStyle.alert)
+            aC.addAction(UIAlertAction(title: "Okay!", style: UIAlertActionStyle.cancel, handler: nil))
+            present(aC, animated: true, completion: nil)
             UserDefaults.standard.set(true, forKey: "launchedBefore")
-        
+            
         }
     }
 
@@ -31,6 +39,10 @@ class LoginVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func loginAction(_ sender: AnyObject) {
+    }
+    
 
 
 }
