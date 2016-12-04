65af2f272b9f2b54f657024daae9814ffcac40c1
John Leonardo <jdleonardo5@icloud.com>
2016-12-03 17:46:20 -0800

    firebase auth and user defaults set up!

--- Find Some Friends/LoginVC.swift
+++ Find Some Friends/LoginVC.swift
@@ -15,6 +15,8 @@ class LoginVC: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
     
+    var userID: String?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +25,33 @@ class LoginVC: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-        if launchedBefore  {
+        if launchedBefore {
             print("Not the first launch")
         } else {
             
             print("First launch, displaying welcome message")
-            let aC = UIAlertController(title: "Welcome", message: "Welcome to FSF! Feel free to login/signup here. If you don't have an account, it will be created automatically. If you do, just sign in as usual. You will then be prompted to set a profile picture. Have fun! -John", preferredStyle: UIAlertControllerStyle.alert)
-            aC.addAction(UIAlertAction(title: "Okay!", style: UIAlertActionStyle.cancel, handler: nil))
-            present(aC, animated: true, completion: nil)
+            showAlert(alertTitle: "Welcome", msg: "Welcome to FSF! Feel free to login/signup here. If you don't have an account, it will be created automatically. If you do, just sign in as usual. You will then be prompted to set a profile picture. Have fun! -John", actionTitle: "Okay!")
             UserDefaults.standard.set(true, forKey: "launchedBefore")
             
+        }
+        
+        if let email = UserDefaults.standard.string(forKey: "email"), let password = UserDefaults.standard.string(forKey: "password") {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil {
+                    print("some error, ill handle it later")
+                } else {
+                    self.userID = user?.uid
+                    self.performSegue(withIdentifier: "goToMain", sender: nil)
+                }
+            })
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "goToMain") {
+            let destination = segue.destination as? MainVC
+            destination?.userID = userID
         }
     }
 
@@ -43,18 +62,59 @@ class LoginVC: UIViewController {
     
     @IBAction func loginAction(_ sender: AnyObject) {
         if let email = emailField.text, let password = passwordField.text {
-            FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
-                if error != nil {
-                    //success
-                    
-                } else {
-                    //there's an error
+            if password.characters.count >= 8 {
+                //password is over 8 chars
+                FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+                    if error != nil {
+                        //some error
+                        if let code = FIRAuthErrorCode(rawValue: (error?._code)!) {
+                            
+                            switch code {
+                            case .errorCodeUserDisabled:
+                                //user is banned
+                                self.showAlert(alertTitle: "Oh No!", msg: "You've been banned! Please contact the admin j@zaap.ws and let's get this resolved", actionTitle: "Okay :(")
+                            case .errorCodeUserNotFound:
+                                //user not found, create account here
+                                FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+                                    if error != nil {
+                                        self.showAlert(alertTitle: "Oops", msg: (error?.localizedDescription)!, actionTitle: "Okay!")
+                                    } else {
+                                        UserDefaults.standard.set(email, forKey: "email")
+                                        UserDefaults.standard.set(password, forKey: "password")
+                                        self.userID = user?.uid
+                                        self.performSegue(withIdentifier: "goToMain", sender: nil)
+                                        
+                                    }
+                                }
+                                
+                            default:
+                                //just show the localized desc. to the user
+                                self.showAlert(alertTitle: "Oops", msg: (error?.localizedDescription)!, actionTitle: "Okay!")
+                            }
+                            
+                        }
+                        
+                    } else {
+                        UserDefaults.standard.set(email, forKey: "email")
+                        UserDefaults.standard.set(password, forKey: "password")
+                        self.userID = user?.uid
+                        self.performSegue(withIdentifier: "goToMain", sender: nil)
+
+                    }
                 }
+            } else {
+                //password is under 8 chars
+                showAlert(alertTitle: "Oops!", msg: "Password must be at least 8 characters", actionTitle: "Okay!")
             }
             
-        } else {
-            //doesnt have both fields filled out
         }
+    
+    }
+    
+    func showAlert(alertTitle: String, msg: String, actionTitle: String) {
+        let aC = UIAlertController(title: alertTitle, message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        aC.addAction(UIAlertAction(title: actionTitle, style: UIAlertActionStyle.cancel, handler: nil))
+        present(aC, animated: true, completion: nil)
     }
     
 
---
+++ Find Some Friends/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainVC.swift
+//  Find Some Friends
+//
+//  Created by John Leonardo on 12/3/16.
+//  Copyright © 2016 John Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    var userID: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(userID)
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
